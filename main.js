55dfdca255dac06809a48e40de8b3644cb2fe715
SONAL MISHRA <sonalmishra@2001>
2024-04-11 22:24:45 +0530

    commit10

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new typed(".text",{
+    String:["Full Stack Web Development","Javascript Developer",  "Worked On MongoDB/MySQl Databases"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
